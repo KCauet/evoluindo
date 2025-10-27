2db9e9322f773b219383ee3268f62d7bfb5c0f60
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-10-27 17:12:06 -0300

    Dependencia funcionando

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,4 +1,5 @@
 import styles from './Music.module.css'
+import { Play } from 'lucide-react'
 
 function MusicPlayer() {
     return (
@@ -7,7 +8,7 @@ function MusicPlayer() {
                 <div className={styles.buttons}>
                     <button>Anterior</button>
                     <button>Play</button>
-                    
+                    <Play size={32}/>
                     <button>Proxima</button>
                 </div>
             </div>
