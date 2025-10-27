49013bcbfb3a03ccab5e8d0b5633c20ef033b707
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-10-27 18:14:38 -0300

    continua

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,10 +1,27 @@
+import { useRef, useState } from 'react'
 import styles from './Music.module.css'
-import { Play, Pause, SkipForward, SkipBack, Volume2, Dam } from 'lucide-react'
+import { Play, Pause, SkipForward, SkipBack } from 'lucide-react'
 
 function MusicPlayer() {
 
-    function play() {
-        alert('Dando play')
+    const audio = useRef()
+    const [isPlaying, setPlaying] = useState(false)
+    const [display1, setDisplayPlay] = useState('block')
+    const [display2, setDisplayPause] = useState('none')
+
+    function playPause() {
+        if (isPlaying) {
+            audio.current.pause()
+            setPlaying(false)
+            setDisplayPlay('block')
+            setDisplayPause('none')
+        } else {
+            audio.current.play()
+            setPlaying(true)
+            setDisplayPlay('none')
+            setDisplayPause('block')
+        }
+        
     }
 
     return (
@@ -12,9 +29,10 @@ function MusicPlayer() {
             <div className={styles.subBox}>
                 <div className={styles.buttons}>
                     
+                    <audio ref={audio} src="/Blue lock.mp3"></audio>
                     <SkipBack size={32}/>
-                    <Play size={32} onClick={play} style={{display: 'none'}}/>
-                    <Pause size={32}/>
+                    <Play size={32} onClick={playPause} display={display1}/>
+                    <Pause size={32} onClick={playPause} display={display2} />
                     <SkipForward size={32}/>
                     
                     
