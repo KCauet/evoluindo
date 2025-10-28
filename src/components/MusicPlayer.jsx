50b8026b89a904ce18ee47a494be26543e212dfa
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-10-28 11:12:57 -0300

    Mais alterações e agora vou fazer o botão mute

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react'
 import styles from './Music.module.css'
-import { Play, Pause, SkipForward, SkipBack } from 'lucide-react'
+import { Play, Pause, SkipForward, SkipBack, Volume2, Volume1, VolumeOff } from 'lucide-react'
 
 function MusicPlayer() {
 
@@ -8,6 +8,7 @@ function MusicPlayer() {
     const [isPlaying, setPlaying] = useState(false)
     const [display1, setDisplayPlay] = useState('block')
     const [display2, setDisplayPause] = useState('none')
+    const [volume, setVolume] = useState(1)
 
     function playPause() {
         if (isPlaying) {
@@ -24,17 +25,37 @@ function MusicPlayer() {
         
     }
 
+    function volumeIcon() {
+
+        if (volume === 0) {
+            return <VolumeOff size={32}/>
+        } else if (volume > 0 && volume < 0.35) {
+            return <Volume1 size={32}/>
+        } else {
+            return <Volume2 size={32}/>
+        }
+    }
+
+    function ajeitarIcone(event) {
+        const novoVolume = event.target.value / 100
+        setVolume(novoVolume)
+        audio.current.volume = novoVolume
+    }
+
     return (
         <div className={styles.mainBox}>
             <div className={styles.subBox}>
                 <div className={styles.buttons}>
                     
-                    <audio ref={audio} src="/Blue lock.mp3"></audio>
+                    <audio ref={audio} src="/musicas/Blue lock.mp3"></audio>
                     <SkipBack size={32}/>
                     <Play size={32} onClick={playPause} display={display1}/>
                     <Pause size={32} onClick={playPause} display={display2} />
                     <SkipForward size={32}/>
-                    
+                    {volumeIcon()}
+                    <div >
+                        <input type="range" onChange={ajeitarIcone}/>
+                    </div>
                     
                 </div>
             </div>
