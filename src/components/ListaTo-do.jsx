de63bdbe56692e5d55f97147bc0031b66c83c13f
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-10-30 09:13:59 -0300

    mecanica de adicionar e remover itens por id

--- src/components/ListaTo-do.jsx
+++ src/components/ListaTo-do.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+
 function ListaTodo() {
+
+    const [tarefa, setTarefa] = useState('')
+    const [Lista, setLista] = useState([])
+
+    function adicionarTarefa() {
+        let texto = tarefa
+        setLista([texto, ...Lista])
+    }
+
     return (
         <div>
-
+            <input type="text" value={tarefa} onChange={event => setTarefa(event.target.value)}/>
+            {tarefa}
+            <button onClick={adicionarTarefa}>Adicionar tarefa</button>
+            <div>{Lista.map((Itens, ID) => (
+                <div key={ID} >
+                    <p>{Itens}</p>
+                    <button onClick={() => {
+                        setLista(Lista.filter((_, i) => i !== ID))
+                    }}>Apagar</button>
+                </div>
+            ))}
+            </div>
         </div>
     )
 }
