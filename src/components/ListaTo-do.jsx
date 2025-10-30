832a1c3bb4a736faec53ceacd7f058f267dba1c4
Keirrison cauet <keirrisoncauet31@gmail.com>
2025-10-30 13:03:06 -0300

    terminando e estilizando, ou tentando estilizar xd

--- src/components/ListaTo-do.jsx
+++ src/components/ListaTo-do.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import styles from './ListaTo-do.module.css'
 
 function ListaTodo() {
 
@@ -7,22 +8,41 @@ function ListaTodo() {
 
     function adicionarTarefa() {
         let texto = tarefa
+        if (texto.trim() === '') {
+            return
+        }
+
         setLista([texto, ...Lista])
+        setTarefa('')
     }
 
     return (
-        <div>
-            <input type="text" value={tarefa} onChange={event => setTarefa(event.target.value)}/>
-            {tarefa}
-            <button onClick={adicionarTarefa}>Adicionar tarefa</button>
-            <div>{Lista.map((Itens, ID) => (
-                <div key={ID} >
-                    <p>{Itens}</p>
-                    <button onClick={() => {
-                        setLista(Lista.filter((_, i) => i !== ID))
-                    }}>Apagar</button>
+        <div className={styles.mainBox}>
+            <div className={styles.subBox}>
+
+                <section>
+                    <input type="text" value={tarefa} onChange={event => setTarefa(event.target.value)}/>
+                    <br/>
+                    <button onClick={adicionarTarefa}>Adicionar tarefa</button>
+                </section>
+                
+                <div className={styles.itens}>{Lista.map((Itens, ID) => (
+                    
+                    <div key={ID} className={styles.card}>
+                        <div>
+                            <p>{Itens}</p>
+                        </div>
+                        <div>
+                            <button onClick={() => {
+                                setLista(Lista.filter((_, i) => i !== ID))
+                            }}>Apagar</button>
+                        </div>
+                        
+                    </div>
+
+                ))}
                 </div>
-            ))}
+
             </div>
         </div>
     )
